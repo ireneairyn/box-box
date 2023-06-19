36d78e6c4554ddef354eaca5d542a16f31abbe31
Irene Aunon <irene@ireneaunion.com>
2023-06-20 00:06:19 +0200

    add font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=League+Spartan&display=swap"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <meta property="og:image" content="/OGimage.png" />
+          <meta property="og:title" content="Box Box" />
+          <meta property="og:description" content="Boxing Challenge Narrator" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
